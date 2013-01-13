d3b162385ab03b67cf6b22ce16132eb72d8e9e2d
David Vydra <david@vydra.net>
2013-01-13 09:28:39 -0800

    test of zip

--- circle1/src/test/scala/com/cologique/scalabits/circle1/review/CollectionsSuite.scala
+++ circle1/src/test/scala/com/cologique/scalabits/circle1/review/CollectionsSuite.scala
@@ -2,6 +2,7 @@ package com.cologique.scalabits.circle1.review
 import org.scalatest.FunSuite
 
 class CollectionsSuite extends FunSuite {
+  
   test("List") {
 
     val list = List()
@@ -11,5 +12,13 @@ class CollectionsSuite extends FunSuite {
     assert(listOfOneInt.iterator.getClass.toString() === "class scala.collection.LinearSeqLike$$anon$1" )
     val yieldedListOfOne = for( i <- listOfOneInt) yield i
     assert( yieldedListOfOne === List(1))
+  }
+  
+  test("zip List[Int] and List[Char]") {
+    val keys = List(1,2)
+    val vals = List('a','b')
+    val map = keys.zip(vals).toMap[Int, Char]
+    assert( map.getClass.toString === "class scala.collection.immutable.Map$Map2")
+    assert(map === Map(1 -> 'a', 2 -> 'b'))
   }
 }

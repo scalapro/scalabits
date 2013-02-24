55f6053e7fc0849ff0974fd6a6748fa00cff9421
David Vydra <david@vydra.net>
2013-02-23 18:45:16 -0800

    added DbTest

--- circle1/src/main/scala/com/cologique/scalabits/circle1/review/Puzzler.scala
+++ circle1/src/main/scala/com/cologique/scalabits/circle1/review/Puzzler.scala
@@ -1,8 +1,20 @@
 package com.cologique.scalabits.circle1.review
+import scala.collection.mutable.ListBuffer
 
-object Puzzler {
+object Puzzler extends App {
+  
+  def fixture =
+    new {
+      val builder = new StringBuilder("ScalaTest is ")
+      val buffer = new ListBuffer[String]
+    }
 
   // What is the type of this variable and why?
   val strange = 1 + null
+  
+  val f1 = fixture
+  f1.builder.append("a")
+  val f2 = fixture
+  println( f1.builder.toString() )
   
 }

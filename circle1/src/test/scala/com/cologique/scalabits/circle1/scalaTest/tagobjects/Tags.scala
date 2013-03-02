f35692088cc7af0d9ac39818459e022c4c726e82
TonyHsieh <tony@hsieh.org>
2013-03-01 21:12:32 -0800

    Added more tests to ScalaLifeTest.scala Also added more comments to the Game of Life code - Life.scala and Board.scala.

---
+++ circle1/src/test/scala/com/cologique/scalabits/circle1/scalaTest/tagobjects/Tags.scala
@@ -0,0 +1,10 @@
+package com.cologique.scalabits.circle1.scalaTest.tagobjects
+
+/* Used for tagging FunSpec it() functions */
+import org.scalatest.Tag
+
+/* Used on the the Position related tests */
+object PositionTest extends Tag("com.cologique.scalabits.circle1.tags.PositionTest")
+
+/*  Used for the entire Life class test */
+object LifeTest extends Tag("com.cologique.scalabits.circle1.tags.LifeTest")
